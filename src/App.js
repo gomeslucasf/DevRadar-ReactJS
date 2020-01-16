48f8b877060dc5bbe7fcd0dcebf0b41d3471b817
Lucas Gomes <gomesfeliplucas@unoeste.edu.sp>
2020-01-16 18:21:39 -0300

    Indo para A missa da Paulinha minha irma <3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,148 @@
 import React from 'react';
-import logo from './logo.svg';
+import './global.css';
 import './App.css';
-
+import './Sidebar.css';
+import './Main.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>
+          Cadastrar
+        </strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/49655656?s=400&v=4" alt="Lucas Gomes"/>
+              <div className="user-info">
+                <strong>  
+                  Lucas Gomes
+                </strong>
+                <span>
+                  Java, C++, C, JavaScript
+                </span>
+              </div>
+            </header>
+            <p>
+              Aluno de Bacharelado em Sistemas de Informação pela FIPP/Unoeste. Estudando a stack ReactJS, React Native e NodeJS, pelo Bootcamp da @Rocketseat.
+            </p>
+            <a href="https://github.com/gomeslucasf">
+              Acessar Perfil no GitHub
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/49655656?s=400&v=4" alt="Lucas Gomes"/>
+              <div className="user-info">
+                <strong>  
+                  Lucas Gomes
+                </strong>
+                <span>
+                  Java, C++, C, JavaScript
+                </span>
+              </div>
+            </header>
+            <p>
+              Aluno de Bacharelado em Sistemas de Informação pela FIPP/Unoeste. Estudando a stack ReactJS, React Native e NodeJS, pelo Bootcamp da @Rocketseat.
+            </p>
+            <a href="https://github.com/gomeslucasf">
+              Acessar Perfil no GitHub
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/49655656?s=400&v=4" alt="Lucas Gomes"/>
+              <div className="user-info">
+                <strong>  
+                  Lucas Gomes
+                </strong>
+                <span>
+                  Java, C++, C, JavaScript
+                </span>
+              </div>
+            </header>
+            <p>
+              Aluno de Bacharelado em Sistemas de Informação pela FIPP/Unoeste. Estudando a stack ReactJS, React Native e NodeJS, pelo Bootcamp da @Rocketseat.
+            </p>
+            <a href="https://github.com/gomeslucasf">
+              Acessar Perfil no GitHub
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/49655656?s=400&v=4" alt="Lucas Gomes"/>
+              <div className="user-info">
+                <strong>  
+                  Lucas Gomes
+                </strong>
+                <span>
+                  Java, C++, C, JavaScript
+                </span>
+              </div>
+            </header>
+            <p>
+              Aluno de Bacharelado em Sistemas de Informação pela FIPP/Unoeste. Estudando a stack ReactJS, React Native e NodeJS, pelo Bootcamp da @Rocketseat.
+            </p>
+            <a href="https://github.com/gomeslucasf">
+              Acessar Perfil no GitHub
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/49655656?s=400&v=4" alt="Lucas Gomes"/>
+              <div className="user-info">
+                <strong>  
+                  Lucas Gomes
+                </strong>
+                <span>
+                  Java, C++, C, JavaScript
+                </span>
+              </div>
+            </header>
+            <p>
+              Aluno de Bacharelado em Sistemas de Informação pela FIPP/Unoeste. Estudando a stack ReactJS, React Native e NodeJS, pelo Bootcamp da @Rocketseat.
+            </p>
+            <a href="https://github.com/gomeslucasf">
+              Acessar Perfil no GitHub
+            </a>
+          </li>
+        </ul>
+      </main>
     </div>
-  );
+  )
 }
 
 export default App;
+
+/***
+ * Componente : Bloco isolado de Html+css+Js, que não afeta outros componentes
+ * 
+ * Estado: A forma como as informaçoes mantem em um componente ja que o react n observa as variaveis
+ * 
+ * Propriedade: Atributos html - Informações do componente pai passa aos filhos 
+ * 
+ */
