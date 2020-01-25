ba17cc0110032afb841409223f407bdade2e5482
Lucas Gomes <gomesfeliplucas@unoeste.edu.sp>
2020-01-25 18:42:33 -0300

    Finalizando front web

--- src/App.js
+++ src/App.js
@@ -1,135 +1,44 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import api from './services/api';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
-import './Main.css'
+import './Main.css';
+import DevItem from './Components/DevItem';
+import DevForm from './Components/DevForm';
+
+
 function App() {
+  const [devs, setDevs]=useState([]);
+  
+  useEffect(()=>{
+    async function loadDevs(){
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    loadDevs();
+  },[])
+
+  async function handleAddDev(data){
+      const response = await api.post('/devs',data);
+      setDevs([...devs, response.data]);
+    }
   return (
     <div id="app">
       <aside>
         <strong>
           Cadastrar
         </strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do GitHub</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/>
-            </div>
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required/>
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/49655656?s=400&v=4" alt="Lucas Gomes"/>
-              <div className="user-info">
-                <strong>  
-                  Lucas Gomes
-                </strong>
-                <span>
-                  Java, C++, C, JavaScript
-                </span>
-              </div>
-            </header>
-            <p>
-              Aluno de Bacharelado em Sistemas de Informação pela FIPP/Unoeste. Estudando a stack ReactJS, React Native e NodeJS, pelo Bootcamp da @Rocketseat.
-            </p>
-            <a href="https://github.com/gomeslucasf">
-              Acessar Perfil no GitHub
-            </a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/49655656?s=400&v=4" alt="Lucas Gomes"/>
-              <div className="user-info">
-                <strong>  
-                  Lucas Gomes
-                </strong>
-                <span>
-                  Java, C++, C, JavaScript
-                </span>
-              </div>
-            </header>
-            <p>
-              Aluno de Bacharelado em Sistemas de Informação pela FIPP/Unoeste. Estudando a stack ReactJS, React Native e NodeJS, pelo Bootcamp da @Rocketseat.
-            </p>
-            <a href="https://github.com/gomeslucasf">
-              Acessar Perfil no GitHub
-            </a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/49655656?s=400&v=4" alt="Lucas Gomes"/>
-              <div className="user-info">
-                <strong>  
-                  Lucas Gomes
-                </strong>
-                <span>
-                  Java, C++, C, JavaScript
-                </span>
-              </div>
-            </header>
-            <p>
-              Aluno de Bacharelado em Sistemas de Informação pela FIPP/Unoeste. Estudando a stack ReactJS, React Native e NodeJS, pelo Bootcamp da @Rocketseat.
-            </p>
-            <a href="https://github.com/gomeslucasf">
-              Acessar Perfil no GitHub
-            </a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/49655656?s=400&v=4" alt="Lucas Gomes"/>
-              <div className="user-info">
-                <strong>  
-                  Lucas Gomes
-                </strong>
-                <span>
-                  Java, C++, C, JavaScript
-                </span>
-              </div>
-            </header>
-            <p>
-              Aluno de Bacharelado em Sistemas de Informação pela FIPP/Unoeste. Estudando a stack ReactJS, React Native e NodeJS, pelo Bootcamp da @Rocketseat.
-            </p>
-            <a href="https://github.com/gomeslucasf">
-              Acessar Perfil no GitHub
-            </a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/49655656?s=400&v=4" alt="Lucas Gomes"/>
-              <div className="user-info">
-                <strong>  
-                  Lucas Gomes
-                </strong>
-                <span>
-                  Java, C++, C, JavaScript
-                </span>
-              </div>
-            </header>
-            <p>
-              Aluno de Bacharelado em Sistemas de Informação pela FIPP/Unoeste. Estudando a stack ReactJS, React Native e NodeJS, pelo Bootcamp da @Rocketseat.
-            </p>
-            <a href="https://github.com/gomeslucasf">
-              Acessar Perfil no GitHub
-            </a>
-          </li>
+          {devs.map(dev => (
+           <DevItem key={dev._id} dev={dev}>
+
+           </DevItem>
+          ))}
+          
         </ul>
       </main>
     </div>
